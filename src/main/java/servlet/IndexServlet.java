48d16fc7e2249ede1ef16cddecca099815b98c6a
Temelius <teme989@gmail.com>
2020-05-21 18:27:44 +0300

    Change Hello World to actual Artist logic

--- src/main/java/servlet/IndexServlet.java
+++ src/main/java/servlet/IndexServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 import java.time.LocalTime;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,15 +10,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import database.JDBCArtistDao;
+import database.ArtistDao;
+import model.Artist;
+
 @WebServlet("")
 public class IndexServlet extends HttpServlet{
 	
+	private ArtistDao dao = new JDBCArtistDao();
+	
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String helloworld = "Hello World!";
+        List<Artist> artists = dao.getAllItems();
 
         // pass the time string to the JSP page as an attribute
-        req.setAttribute("helloworld", helloworld);
+        req.setAttribute("items", artists);
 
         // forward the request to the index.jsp page
         req.getRequestDispatcher("/WEB-INF/index.jsp").forward(req, resp);
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -1,13 +1,33 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<title>Artists</title>
-	<link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">
+<meta charset="UTF-8">
+<title>Artists</title>
+<link rel="stylesheet"
+	href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">
 </head>
 <body>
-	<h1>${ helloworld }</h1>
+	<h1>🎤 Artists</h1>
+	<table>
+		<thead>
+			<th>#</th>
+			<th>Artist</th>
+		</thead>
+		<tbody>
+			<c:forEach items="${ items }" var="artist">
+				<tr>
+					<td>
+						<c:out value="${ artist.getArtistId() }"></c:out>
+					</td>
+					<td>
+						<c:out value="${ artist.getArtistName() }"></c:out>
+					</td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
 </body>
 </html>
