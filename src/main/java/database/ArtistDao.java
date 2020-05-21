6e750f25115eff2537910f128b89827376fd82af
Temelius <teme989@gmail.com>
2020-05-21 03:53:52 +0300

    Create Artist class and ArtistDao interface

---
+++ src/main/java/database/ArtistDao.java
@@ -0,0 +1,20 @@
+package database;
+
+import java.util.List;
+
+import model.Artist;
+
+/**
+ * ArtistDao on rajapinta, joka määrittelee, mitä operaatioita
+ * DAO-luokan on toteutattava.
+ */
+
+public interface ArtistDao {
+    public List<Artist> getAllItems();
+
+    public Artist getArtist(long id);
+
+    public boolean addArtist(Artist newArtist);
+
+    public boolean removeArtist(Artist artist);
+}
