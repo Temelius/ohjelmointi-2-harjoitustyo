2a3244be11bfe0a7356c381ae4601cb5e637ab9f
Temelius <teme989@gmail.com>
2020-05-24 05:25:51 +0300

    Add remove album and fixes

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,5 +1,15 @@
 async function removeArtist(id) {
-    let res = await fetch(`/?id=${id}`, 
+    let res = await fetch(`/artists?id=${id}`, 
+ 		   {method: 'DELETE'}) 
+    .then(res => {
+    	var rowId = `artist-${id}`;
+    	var myObj = document.getElementById(rowId);
+    	myObj.remove();
+    })  	
+}
+
+async function removeAlbum(id) {
+    let res = await fetch(`/albums?id=${id}`, 
  		   {method: 'DELETE'}) 
     .then(res => {
     	var rowId = `album-${id}`;
